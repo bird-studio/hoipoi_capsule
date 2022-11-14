1cec0fa3804a086c8820a8da7f6274042005a523
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-15 00:40:15 +0900

    :bug:[x.x.o]: Fix debug code

--- src/external_interface/git.ts
+++ src/external_interface/git.ts
@@ -2,8 +2,6 @@ import * as path from "@/path/mod.ts";
 import * as deno from "./deno.ts";
 import * as error from "~/src/util/error.ts";
 
-await Deno.writeTextFile("./a.txt", "Hello World!");
-
 const commitEditMsgPath = path.join(
   ".git",
   "COMMIT_EDITMSG",
