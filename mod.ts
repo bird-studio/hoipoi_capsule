f16ca13b28e2bb1c97a5b9f1222e98cf742a592e
鳥山明 <92862731+akira-toriyama@users.noreply.github.com>
2022-11-16 00:52:18 +0900

    :memo:[x.x.x]: Update README (#7)
    * :memo:[x.x.x]: Update README
    
    * :sparkles:[x.o.x]: Export preset

--- mod.ts
+++ mod.ts
@@ -1,13 +1,10 @@
-// external_interface
 import * as git from "~/src/external_interface/git.ts";
 import * as gitHub from "~/src/external_interface/gitHub.ts";
 import * as gitmoji from "~/src/external_interface/gitmoji.ts";
-
-// user_interface
 import * as prompt from "~/src/user_interface/prompt.ts";
-
-// use_case
 import * as fillInCommitMessage from "~/src/use_case/fill_in_commit_message/mod.ts";
+import * as conventionalcommits from "~/src/use_case/fill_in_commit_message/conventionalcommits/mod.ts";
+import * as gitmojiStyle from "~/src/use_case/fill_in_commit_message/gitmoji_style/mod.ts";
 
 export const externalInterface = {
   git,
@@ -21,4 +18,9 @@ export const userInterface = {
 
 export const useCase = {
   fillInCommitMessage,
+} as const;
+
+export const preset = {
+  conventionalcommits,
+  gitmojiStyle,
 } as const;
