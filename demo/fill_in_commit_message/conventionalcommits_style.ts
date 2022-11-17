fbef43f4d90d5043d73152522ad194c3e120bf8a
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-17 11:32:16 +0900

    :technologist:[x.x.x]: Fix demo

--- demo/fill_in_commit_message/conventionalcommits_style.ts
+++ demo/fill_in_commit_message/conventionalcommits_style.ts
@@ -1,6 +1,6 @@
 //  deno run --allow-net --allow-write --allow-run demo/fill_in_commit_message/conventionalcommits_style.ts
 
-import { preset, useCase } from "../../mod.ts";
+import * as hoipoiCapsule from "../../mod.ts";
 
 const commitMessageTemplate = `{{type}}({{scope}}): {{summary}}
 
@@ -8,35 +8,41 @@ const commitMessageTemplate = `{{type}}({{scope}}): {{summary}}
 
 BREAKING CHANGE: {{breakingChange}}`;
 
-useCase.fillInCommitMessage.run({
+hoipoiCapsule.useCase.fillInCommitMessage.run({
   commitMessageTemplate,
   questionList: [
     {
       target: "type",
-      q: preset.fillInCommitMessage.conventionalcommits.qMap.type,
+      q: hoipoiCapsule.preset.fillInCommitMessage.conventionalcommits.qMap.type,
     },
     {
       target: "scope",
-      q: preset.fillInCommitMessage.conventionalcommits.qMap.scope,
+      q: hoipoiCapsule.preset.fillInCommitMessage.conventionalcommits.qMap
+        .scope,
       fixCommitMessage:
-        preset.fillInCommitMessage.conventionalcommits.fixCommitMessageMap
+        hoipoiCapsule.preset.fillInCommitMessage.conventionalcommits
+          .fixCommitMessageMap
           .scope,
     },
     {
       target: "summary",
-      q: preset.fillInCommitMessage.conventionalcommits.qMap.summary,
+      q: hoipoiCapsule.preset.fillInCommitMessage.conventionalcommits.qMap
+        .summary,
     },
     {
       target: "body",
-      q: preset.fillInCommitMessage.conventionalcommits.qMap.body,
+      q: hoipoiCapsule.preset.fillInCommitMessage.conventionalcommits.qMap.body,
       fixCommitMessage:
-        preset.fillInCommitMessage.conventionalcommits.fixCommitMessageMap.body,
+        hoipoiCapsule.preset.fillInCommitMessage.conventionalcommits
+          .fixCommitMessageMap.body,
     },
     {
       target: "breakingChange",
-      q: preset.fillInCommitMessage.conventionalcommits.qMap.breakingChange,
+      q: hoipoiCapsule.preset.fillInCommitMessage.conventionalcommits.qMap
+        .breakingChange,
       fixCommitMessage:
-        preset.fillInCommitMessage.conventionalcommits.fixCommitMessageMap
+        hoipoiCapsule.preset.fillInCommitMessage.conventionalcommits
+          .fixCommitMessageMap
           .breakingChange,
     },
   ],
