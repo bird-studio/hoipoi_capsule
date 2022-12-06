2880e2195ae8b381b3495c1c0bab7da4f57cbbbb
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-12-06 16:23:26 +0900

    :alembic:[x.x.o]: Add debug code

--- src/external_interface/findConfig.ts
+++ src/external_interface/findConfig.ts
@@ -1,6 +1,7 @@
+import { parse } from "https://deno.land/std@0.66.0/flags/mod.ts";
 import { Config } from "../type.ts";
 
-const configFile = `${Deno.cwd()}/hoipoi_capsule.config.ts`;
+// const configFile = "~/hoipoi_capsule.config.ts";
 
 type FindConfig = () => Promise<
   null | {
@@ -8,11 +9,15 @@ type FindConfig = () => Promise<
   }
 >;
 export const findConfig: FindConfig = () =>
-  import(configFile).then((v) => {
-    console.log({ configFile });
+  import(parse(Deno.args).a).then((v) => {
+    // console.log({ configFile });
     console.log({ v });
     return v;
   }).catch((e) => {
-    console.log({ configFile });
+    // console.log({ configFile });
     console.log({ e });
   });
+
+// parse(Deno.args).a;
+// console.log(parsedArgs.a);
+// ❯ deno run "/Volumes/workspace/bird-studio/hoipoi_capsule/src/external_interface/findConfig.ts" --a="/Volumes/workspace/bird-studio/hoipoi_capsule/hoipoi_capsule.config.ts"
