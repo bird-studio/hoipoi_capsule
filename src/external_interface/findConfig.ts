243ebd9ce0b4819d3a41f183c811e3a01acab511
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-12-06 16:05:16 +0900

    :mute:[x.x.x]: Add log

--- src/external_interface/findConfig.ts
+++ src/external_interface/findConfig.ts
@@ -2,12 +2,13 @@ import { Config } from "../type.ts";
 
 const configFile = `${Deno.cwd()}/hoipoi_capsule.config.ts`;
 
-console.log({ configFile });
-
 type FindConfig = () => Promise<
   null | {
     config: Config;
   }
 >;
 export const findConfig: FindConfig = () =>
-  import(configFile).catch(() => null);
+  import(configFile).then((v) => {
+    console.log({ v });
+    return v;
+  }).catch(() => null);
