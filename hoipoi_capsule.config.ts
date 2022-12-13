218bd71c5359a8b899d7e9afb804867dff0a4c8f
鳥山明 <92862731+akira-toriyama@users.noreply.github.com>
2022-12-13 21:55:31 +0900

    :fire:[x.x.x]: Remove dead code (#27)

--- hoipoi_capsule.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config, userInterface } from "./mod.ts";
-
-export const config: Config = {
-  color: {
-    border: userInterface.prompt.colors.green,
-    target: userInterface.prompt.colors.bold.green,
-  },
-};
