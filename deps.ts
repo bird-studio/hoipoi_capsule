0b8c465760060dea0caba203c16d1c70fd1f1d77
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-01-02 00:23:08 +0900

    :arrow_up:[x.x.o]: bump

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as path from "https://deno.land/std@0.157.0/path/mod.ts";
-export * as prompt from "https://deno.land/x/cliffy@v0.25.4/prompt/mod.ts";
-export { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/colors.ts";
-export { Table } from "https://deno.land/x/cliffy@v0.25.4/table/mod.ts";
+export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
 export { wait } from "https://deno.land/x/wait@0.1.12/mod.ts";
+export * as prompt from "https://deno.land/x/cliffy@v0.25.6/prompt/mod.ts";
+export { colors } from "https://deno.land/x/cliffy@v0.25.6/ansi/colors.ts";
+export { Table } from "https://deno.land/x/cliffy@v0.25.6/table/mod.ts";
