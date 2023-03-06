dd0e52f6be5c708fa2f71bc24f20bf16da34a0fa
鳥山明 <92862731+akira-toriyama@users.noreply.github.com>
2023-03-06 20:37:00 +0900

    :arrow_up:[x.x.o]: bump (#29)

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.178.0/path/mod.ts";
 export { wait } from "https://deno.land/x/wait@0.1.12/mod.ts";
 export * as prompt from "https://deno.land/x/cliffy@v0.25.6/prompt/mod.ts";
 export { colors } from "https://deno.land/x/cliffy@v0.25.6/ansi/colors.ts";
