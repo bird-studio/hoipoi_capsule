7bb77c9b08c32e9d9205f8f5646b4a53a9364d71
鳥山明 <92862731+akira-toriyama@users.noreply.github.com>
2023-08-18 15:30:47 +0900

    :arrow_up:[x.x.o](_): bump (#33)
    * :arrow_up:[x.x.o](_): bump
    
    * :arrow_up:[x.x.o](_): bump

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as path from "https://deno.land/std@0.178.0/path/mod.ts";
-export { wait } from "https://deno.land/x/wait@0.1.12/mod.ts";
+export * as path from "https://deno.land/std@0.198.0/path/mod.ts";
+export { wait } from "https://deno.land/x/wait@0.1.13/mod.ts";
 export * as prompt from "https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts";
 export { colors } from "https://deno.land/x/cliffy@v0.25.7/ansi/colors.ts";
 export { Table } from "https://deno.land/x/cliffy@v0.25.7/table/mod.ts";
