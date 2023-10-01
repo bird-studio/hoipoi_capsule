dbd1bdcf9f114ecdfb8db25aa1b261532a83d9fe
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-10-01 16:06:57 +0900

    Fix/file path (#34)
    * :alien:[x.x.o](gitmoji_style): Fix file path
    
    * :package:[x.x.o](_): Update lock file

--- src/external_interface/gitmoji.ts
+++ src/external_interface/gitmoji.ts
@@ -2,7 +2,7 @@ import * as error from "../util/error.ts";
 
 export const fetchGitmojis = () =>
   import(
-    "https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json",
+    "https://raw.githubusercontent.com/carloscuesta/gitmoji/master/packages/gitmojis/src/gitmojis.json",
     { assert: { type: "json" } }
   ).then((v) => v.default).catch(
     error.throwError,
